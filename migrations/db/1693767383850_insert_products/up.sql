86047c5cd96d1063dd66f408180f051cbbb1b1f5
samiur <samiur@synesisit.info>
2023-09-29 04:25:13 +0600

    initial commit

---
+++ migrations/db/1693767383850_insert_products/up.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (name, description, price)
+VALUES
+    ('Apple', 'A crisp and juicy fruit', '0.75'),
+    ('Tesla Model 3', 'Electric car with advanced features', '45000'),
+    ('Leather sofa', 'Luxurious and comfortable', '799.99'),
+    ('4K Smart TV', 'Ultra-high-definition with smart features', '899.99');
---
+++ migrations/db/1694336855094_insert_products/up.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (name, description, price)
+VALUES
+    ('Orange', 'A citrus fruit with a refreshing taste', '0.80'),
+    ('Porsche 911', 'A high-performance sports car', '120000'),
+    ('Comfortable recliner', 'Perfect for relaxing evenings', '499.99'),
+    ('Curved 4K Monitor', 'Immersive display for productivity', '799.99');
